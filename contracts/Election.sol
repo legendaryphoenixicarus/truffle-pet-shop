f16b8e9058c7153554f8dbc550ab35815eb7dc7b
legendaryphoenixicarus <legendary.phoenix.icarus@gmail.com>
2021-10-16 14:56:46 +0300

    update: add the ability to cast votes in the election

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -15,7 +15,7 @@ contract Election {
     // Store Candidates Count
     uint public candidatesCount;
 
-    constructor() public {
+    constructor() {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
@@ -23,5 +23,22 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+     // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 }
